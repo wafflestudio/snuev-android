21359b14970d4785c928cdd2be0e25afca9382ca
onemonster <onemonsterceo@gmail.com>
2018-10-13 17:56:41 +0900

    Added leak canary

--- app/src/main/java/com/wafflestudio/snuev/SnuevApplication.kt
+++ app/src/main/java/com/wafflestudio/snuev/SnuevApplication.kt
@@ -2,6 +2,7 @@ package com.wafflestudio.snuev
 
 import android.app.Application
 import android.content.Intent
+import com.squareup.leakcanary.LeakCanary
 import com.wafflestudio.snuev.network.SnuevApi
 import com.wafflestudio.snuev.preference.SnuevPreference
 import com.wafflestudio.snuev.view.signin.SignInActivity
@@ -9,6 +10,12 @@ import com.wafflestudio.snuev.view.signin.SignInActivity
 class SnuevApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
         SnuevPreference.init(this)
         SnuevApi.init(this)
     }
